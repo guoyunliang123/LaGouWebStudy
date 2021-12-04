6d991e585d8db4a87161f181a48b9fa224190e85
郭云亮 <1060950677@qq.com>
2021-12-04 11:18:12 +0800

    feat：闭包

--- part1/fed-e-task-01-01/code/02-higher-order-function.js
+++ part1/fed-e-task-01-01/code/02-higher-order-function.js
@@ -15,21 +15,21 @@
 //      lodash中，once 是对一个函数只执行一次。
 
 // once
-// function once (fn) {
-//   let done = false;
-//   return function () {
-//     if(!done) {
-//       done = true;
-//       return fn.apply(this, arguments);
-//     }
-//   }
-// }
-//
-// let pay = once(function(money) {
-//   console.log(`支付了 ${money} RMB`);
-// });
-//
-// pay(5);
-// pay(5);
-// pay(5);
-// pay(5);
+function once (fn) {
+  let done = false;
+  return function () {
+    if(!done) {
+      done = true;
+      return fn.apply(this, arguments);
+    }
+  }
+}
+
+let pay = once(function(money) {
+  console.log(`支付了 ${money} RMB`);
+});
+
+pay(5);
+pay(5);
+pay(5);
+pay(5);
--- part1/fed-e-task-01-01/code/03-arr-method.js
+++ part1/fed-e-task-01-01/code/03-arr-method.js
@@ -3,5 +3,59 @@
 // map
 const map = (array, fn) => {
   const results = [];
-
+  for (let value of array) {
+    results.push(fn(value))
+  }
+  return results;
 }
+
+// 测试
+// let arr = [1, 3, 5, 8, 10];
+// arr = map(arr, v => v * v)
+// console.log(arr)
+
+
+// every
+const every = (array, fn) => {
+  let result = true;
+  for (let value of array) {
+    result = fn(value);
+    if (!result) {
+      break
+    }
+  }
+  return result;
+}
+
+// 测试
+// let arr = [9, 12, 16];
+// let r = every(arr, v => v > 10);
+// console.log(r);
+
+
+// some
+const some = (array, fn) => {
+  let result = false;
+  for (let value of array) {
+    result = fn(value);
+    if (result) {
+      break;
+    }
+  }
+  return result;
+}
+
+// 测试
+// let arr = [1, 5, 7, 11];
+// let r = some(arr, v => v > 10);
+// console.log(r);
+
+
+// sort
+const sort = (array, fn) => {
+  for (let i = 0; i < array.length; i++) {
+
+  }
+}
+
+let arr = [10, 7, 9, 8];
