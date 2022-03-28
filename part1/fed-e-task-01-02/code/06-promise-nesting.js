b288d741f4e81f3bff7059185656957971639ac8
郭云亮 <1060950677@qq.com>
2022-03-28 17:23:01 +0800

    feat：promise 误区嵌套使用

---
+++ part1/fed-e-task-01-02/code/06-promise-nesting.js
@@ -0,0 +1,26 @@
+// Promise 方式的 AJAX
+
+function ajax(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', url) // 请求方式
+    xhr.responseType = 'json'
+    xhr.onload = function () {
+      if (this.status === 200) {
+        resolve(this.response)
+      } else {
+        reject(new Error(this.statusText))
+      }
+    }
+    xhr.send()
+  })
+}
+
+// 嵌套使用时 promise 最常见的误区
+ajax('./api/users.json').then(function (urls) {
+  ajax(urls.users).then(function (users) {
+
+  })
+}, function (error) {
+  console.log(error)
+})
