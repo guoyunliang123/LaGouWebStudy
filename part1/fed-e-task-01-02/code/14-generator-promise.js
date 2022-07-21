5d6442a36dffd5ed7994f259a6c4abb01e4d2297
郭云亮 <1060950677@qq.com>
2022-07-21 16:56:36 +0800

    feat：Generator 配合 promise 的异步解决方案

--- part1/fed-e-task-01-02/code/14-generator-promise.js
+++ part1/fed-e-task-01-02/code/14-generator-promise.js
@@ -17,30 +17,67 @@ function ajax(url) {
 }
 
 function * main() {
-    const users = yield ajax('./api/users.json')
-    console.log(users)
+    try {
+        const users = yield ajax('./api/users.json')
+        console.log(users)
 
-    const posts = yield ajax('/api/posts.json')
-    console.log(posts)
+        const posts = yield ajax('/api/posts.json')
+        console.log(posts)
+
+        const urls = yield ajax('/api/urls111.json')
+        console.log(urls)
+    } catch (e) {
+        console.log(e)
+    }
 }
 
-const g = main();
+// const g = main();
 
-const result = g.next();
+// 递归执行 Generator 函数
+// function handleResult(result) {
+//     if(result.done) return // 生成器函数结束
+//     result.value.then(data =>{
+//         handleResult(g.next(data))
+//     }, error => {
+//         g.throw(error)
+//     })
+// }
+//
+// handleResult(g.next())
 
-result.value.then(data => {
-    const result2 = g.next(data)
+// 生成器函数
+function co(generator) {
+    const g = generator();
 
-    if(result2.done) return;
-
-    result2.value.then(data => {
-        const result3 = g.next(data)
-
-        if(result3.done) return;
-
-        result3.value.then(data => {
-            g.next(data)
+    function handleResult(result) {
+        if(result.done) return // 生成器函数结束
+        result.value.then(data =>{
+            handleResult(g.next(data))
+        }, error => {
+            g.throw(error)
         })
-        g.next(data)
-    })
-})
+    }
+
+    handleResult(g.next())
+}
+
+co(main);
+
+// const result = g.next();
+//
+// result.value.then(data => {
+//     const result2 = g.next(data)
+//
+//     if(result2.done) return;
+//
+//     result2.value.then(data => {
+//         const result3 = g.next(data)
+//
+//         if(result3.done) return;
+//
+//         result3.value.then(data => {
+//             g.next(data)
+//         })
+//         g.next(data)
+//     })
+// })
