d47d7f00a0c980c7f485833f6d4341b23eeef8b6
郭云亮 <1060950677@qq.com>
2022-11-09 17:27:28 +0800

    feat：包含 min 函数的栈

---
+++ part8/fed-e-task-01-01/code/Stack/02-contains-minFun-stack.js
@@ -0,0 +1,42 @@
+// 在存储数据的栈外，再新建一个栈，用于存储最小值
+class MinStack {
+    constructor() {
+        // stackA 用于存储数据
+        this.stackA = [];
+        this.countA = 0;
+        // stackB 用于将数据降序存储（栈顶值为最小值）
+        this.stackB = [];
+        this.countB = 0;
+    }
+
+    // 入栈
+    push(item) {
+        // stackA 正常入栈
+        this.stackA[this.countA++] = item;
+        // stackB 如果没有数据，直接入栈
+        // 如果 item 的值 <= stackB 的最小值，入栈
+        if(this.countB === 0 || item <= this.min()) {
+            this.stackB[this.countB++] = item;
+        }
+    }
+
+    // 最小值函数
+    min() {
+        return this.stackB[this.countB - 1]
+    }
+
+    // 获取栈顶值
+    top() {
+        return this.stackA[this.countA - 1]
+    }
+
+    // 出栈
+    pop() {
+        // 先进行 starkB 的检测
+        // 如果 starkA 的栈顶值 === starkB 的栈顶值，stackB 出栈
+        if(this.top() === this.min()) {
+            delete this.stackB[--this.countB]
+        }
+        delete this.countA[--this.countA]
+    }
+}
