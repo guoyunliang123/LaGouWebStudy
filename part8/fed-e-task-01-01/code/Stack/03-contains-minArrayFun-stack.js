800721af57ca091dcc6f971de008a0d8d5971693
郭云亮 <1060950677@qq.com>
2022-11-10 10:27:39 +0800

    feat: 使用数组内置方法实现获取栈的最小值

---
+++ part8/fed-e-task-01-01/code/Stack/03-contains-minArrayFun-stack.js
@@ -0,0 +1,21 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+    }
+    // 入栈
+    push(item) {
+        this.stack.push(item)
+    }
+    // 查看栈顶值
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+    // 实现最小值功能
+    min() {
+        return Math.min.apply(null, this.stack)
+    }
+    // 出栈方法
+    pop() {
+        this.stack.pop()
+    }
+}
